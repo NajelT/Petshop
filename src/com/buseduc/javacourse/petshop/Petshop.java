0a39af0e5bafcb269a83e0c699ebd4ea5990cfe4
Vadim Semenov <vsemenov@leongaming.com>
2020-08-12 19:44:30 +0300

    prax 0

--- src/com/buseduc/javacourse/petshop/Petshop.java
+++ src/com/buseduc/javacourse/petshop/Petshop.java
@@ -1,16 +1,15 @@
 package com.buseduc.javacourse.petshop;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 public class Petshop {
     public static void main(String[] args) {
-        Settings shopSettings = new Settings();
-        Properties properties = shopSettings.readProperties();
-        String shopName = properties.getProperty("shop.name");
+        Settings shopSettings = Settings.getInstance();
+        String shopName = shopSettings.getShopName();
         System.out.println("Pet Shop: " + shopName );
+        System.out.println("Pet nicks: " + shopSettings.getNicks());
+        System.out.println("Pet specs: " + shopSettings.getSpecies());
+        System.out.println("Pet sexs: " + shopSettings.getSexes());
 
     }
 }
--- src/com/buseduc/javacourse/petshop/Settings.java
+++ src/com/buseduc/javacourse/petshop/Settings.java
@@ -3,10 +3,16 @@ package com.buseduc.javacourse.petshop;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 public class Settings {
-    public Properties readProperties() {
+    private static final Settings settings = new Settings();
+    private Properties properties;
+    private Settings() { }
+
+    private Properties readProperties() {
         Properties properties = new Properties();
         try (InputStream input = new FileInputStream("conf/petshop.properties")) {
             properties.load(input);
@@ -14,6 +20,41 @@ public class Settings {
             System.out.println("Не смогли прочитать настройки");
         }
         return properties;
+    }
+    public String getShopName() {
+        return getProperties().getProperty("shop.name");
 
+    }
+
+
+    public List<String> getNicks() {
+        String nicksStr = getProperties().getProperty("shop.pets.nicks");
+        return Arrays.asList(nicksStr.split(","));
+    }
+
+    public List<String> getSpecies() {
+        String specStr = getProperties().getProperty("shop.pets.species");
+        return Arrays.asList(specStr.split(","));
+    }
+
+    public List<String> getSexes() {
+        String sexStr = getProperties().getProperty("shop.pets.sex");
+        return Arrays.asList(sexStr.split(","));
+    }
+
+    public List<String> getPrices() {
+        String priceStr = getProperties().getProperty("shop.pets.prices");
+        return Arrays.asList(priceStr.split(","));
+    }
+
+    private Properties getProperties() {
+        if (properties == null) {
+            properties = readProperties();
+        }
+        return properties;
+    }
+
+    public static Settings getInstance() {
+        return settings;
     }
 }
